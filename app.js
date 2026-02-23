3dd9e3b5681233e83adc15b64146522a9d105b45
manuelgarciafrr-create <manuelgarciafrr@gmail.com>
2026-02-23 19:59:12 -0400

    Actualizar app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const PRODUCTOS_DATA = [
     {
         id: 1,
         nombre: "CAMISETA OVERSIZE MÉTRICA FRAGMENT",
-        precio: "21",
+        precio: "20",
         imagenes: ["imágenes/fragmentdelante.png", "imágenes/fragmentdetras.png"], 
         colores: ["Negro", "Blanco"],
         tallas: ["S", "M", "L", "XL"]
@@ -24,7 +24,7 @@ const PRODUCTOS_DATA = [
     {
         id: 2,
         nombre: "CAMISETA OVERSIZE MÉTRICA AURA VISUAL",
-        precio: "21",
+        precio: "20",
         imagenes: ["imagenes/auradelante.png", "imagenes/auradetras.png"],
         colores: ["Negro", "Blanco" ,"Gris"],
         tallas: ["S", "M", "L", "XL"]
